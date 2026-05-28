deed44e7180f79bab8a79f67885be2a5e2afd219
Yadav-SubratKumar <yadavsubrat007@gmail.com>
2026-05-28 19:17:16 +0530

    Add: Gateway Api and HttpRoute for Application and Observebility

--- automation/obervability.sh
+++ automation/obervability.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-helm repo add argo https://argoproj.github.io/argo-helm
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-
-
+helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml
+helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric --create-namespace -n nginx-gateway
 
-kubectl create namespace argocd
-kubectl create namespace monitoring
 
-
-helm upgrade --install argocd argo/argo-cd --namespace argocd
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring
-helm upgrade ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace -f values.yaml
+helm upgrade --install argocd argo/argo-cd --namespace argocd --create-namespace
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+# helm upgrade ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace -f values.yaml # only for observabitlity 
 
 
 kubectl patch svc prometheus-kube-prometheus-prometheus -n monitoring -p '{"spec": {"type": "NodePort"}}'
